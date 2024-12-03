35d878b48ecd029fe5d5a14bce9a4c5ec2ae6300
unknown <khalilrami003@gmail.com>
2024-12-03 21:43:02 +0100

    inclusion of zeep

--- project/tests/populate_soap.py
+++ project/tests/populate_soap.py
@@ -4,4 +4,8 @@ from zeep import Client
 wsdl = "http://localhost:5000/soap/?wsdl"  # For example, 'http://example.com/service?wsdl'
 client = Client(wsdl=wsdl)
 
-print(client.service)
+#print(dir(client.service))
+#response = client.service.add_doctor(name="Dr. Smith", specialty="Cardiology")
+#print(response)
+rep = client.service.get_doctor(doctor_id=1)
+print(rep)

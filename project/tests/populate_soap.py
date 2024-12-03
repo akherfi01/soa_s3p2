9fca767096a4ce4ef558c6a66e2b24b945df6678
reda <redaakherfi3@gmail.com>
2024-12-03 21:23:35 +0100

    added the zeep test

---
+++ project/tests/populate_soap.py
@@ -0,0 +1,7 @@
+from zeep import Client
+
+# Replace with the actual WSDL URL or file path
+wsdl = "http://localhost:5000/soap/?wsdl"  # For example, 'http://example.com/service?wsdl'
+client = Client(wsdl=wsdl)
+
+print(client.service)

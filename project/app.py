1ae3cff399bcb7e555127c645e72b18d26631860
reda <redaakherfi3@gmail.com>
2024-12-07 12:42:58 +0100

    modifying the port

--- project/app.py
+++ project/app.py
@@ -42,4 +42,4 @@ def home():
     return "Bienvenue sur l'application combinée REST et SOAP."
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=54321)
+    app.run(debug=True)
